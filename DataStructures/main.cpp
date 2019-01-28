251468a0e56909516d158effa81dafef28666967
squidlywizard <ztdenna@hotmail.com>
2019-01-28 08:46:48 -0700

    Initial Commit

---
+++ DataStructures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by Denna, Zachary on 1/28/19.
+//  Copyright © 2019 Denna, Zachary. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
