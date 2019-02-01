5521116b1f4fe3342ad157806879f238c6d009eb
squidlywizard <ztdenna@hotmail.com>
2019-02-01 08:20:06 -0700

    added the "List"

---
+++ DataStructures/Model/Linear/List.h
@@ -0,0 +1,32 @@
+//
+//  List.h
+//  DataStructures
+//
+//  Created by Denna, Zachary on 2/1/19.
+//  Copyright © 2019 Denna, Zachary. All rights reserved.
+//
+
+#ifndef List_h
+#define List_h
+#include <assert.h>
+#include "../../Nodes/LinearNodes.hpp"
+
+class List
+{
+protected:
+    int size;
+public:
+    //Structure
+    virtual void add(Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
+    virtual Type remove(int index) = 0;
+    virtual Type getFromIndex(int index) = 0;
+    //Helper
+    virtual int getSize() const = 0;
+    virtual LinearNode<Type> * getFront() = 0;
+    virtual LinearNode<Type> * getEnd() = 0;
+    
+    
+}
+
+#endif /* List_h */
