e1d8443d0a4c646181036a48edf5cdc8eb50e115
squidlywizard <ztdenna@hotmail.com>
2019-02-05 09:23:07 -0700

    moved some stuff

---
+++ DataStructures/Controller/FileController.cpp
@@ -0,0 +1,10 @@
+//
+//  FileController.cpp
+//  DataStructures
+//
+//  Created by Denna, Zachary on 2/5/19.
+//  Copyright © 2019 Denna, Zachary. All rights reserved.
+//
+
+#include "FileController.hpp"
+
---
+++ DataStructures/Controller/FileController.hpp
@@ -0,0 +1,28 @@
+//
+//  FileController.hpp
+//  DataStructures
+//
+//  Created by Denna, Zachary on 2/5/19.
+//  Copyright © 2019 Denna, Zachary. All rights reserved.
+//
+
+#ifndef FileController_hpp
+#define FileController_hpp
+
+#include <stdio.h>
+#include "../Resources/Music.hpp"
+#include "../Resources/CrimeData.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+class FileController
+{
+public:
+    static vector<CrimeData> readCrimeDataToVector(string filename);
+    static vector<Music> musicDataToVector(string filename);
+};
+#endif /* FileController_hpp */
