e47a25d78ab9f86d8464635f032b92501e7ef2c1
squidlywizard <ztdenna@hotmail.com>
2019-02-05 09:33:11 -0700

    fileController is done

--- DataStructures/Controller/FileController.cpp
+++ DataStructures/Controller/FileController.cpp
@@ -8,3 +8,71 @@
 
 #include "FileController.hpp"
 
+vector<CrimeData> FileController:: readCrimeDataToVector(string filename)
+{
+    std:: vector<CrimeData> crimeVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename);
+    
+    if (dataFile.is_open())
+    {
+        while (!dataFile.eof())
+        {
+            getline(dataFile, currentCSVLine, '\r');
+            
+            if (rowCount != 0)
+            {
+                if(currentCSVLine.length() != 0)
+                {
+                    CrimeData row(currentCSVLine);
+                    crimeVector.push_back(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "No File" << endl;
+        
+    }
+    
+    return crimeVector;
+}
+vector<Music> FileController:: musicDataToVector(string filename)
+{
+    std:: vector<Music> musicVector;
+    string currentCSVLine;
+    int rowCount = 0;
+    
+    ifstream dataFile(filename);
+    
+    if (dataFile.is_open())
+    {
+        while (!dataFile.eof())
+        {
+            getline(dataFile, currentCSVLine, '\r');
+            
+            if (rowCount != 0)
+            {
+                if(currentCSVLine.length() != 0)
+                {
+                    Music row(currentCSVLine);
+                    musicVector.push_back(row);
+                }
+            }
+            rowCount++;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "No File" << endl;
+        
+    }
+    
+    return musicVector;
+}
