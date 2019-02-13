677fc7af9ef61e9a2a73268492c67db7abab2604
squidlywizard <ztdenna@hotmail.com>
2019-02-13 09:10:17 -0700

    updated FileController

--- DataStructures/Controller/FileController.hpp
+++ DataStructures/Controller/FileController.hpp
@@ -17,12 +17,21 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include "../Model/Linear/Array.hpp"
+#include "../Model/Linear/LinkedList.hpp"
 using namespace std;
 
 class FileController
 {
 public:
     static vector<CrimeData> readCrimeDataToVector(string filename);
+    static LinkedList<CrimeData> readDataToList(string filename);
+    
+    
     static vector<Music> musicDataToVector(string filename);
+    
+    static Array<Music> musicDataToArray(string filename);
+    static LinkedList<Music> musicDataToList(string filename);
+    
 };
 #endif /* FileController_hpp */
